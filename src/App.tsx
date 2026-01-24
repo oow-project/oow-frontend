c53ace258a5a7872e3ab01852718ac11adc08043
jwe0516 <jwe0516@naver.com>
2026-01-24 16:11:30 +0900

    feat: Layout 컴포넌트 추가 및 App에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Nav } from "./components/Nav";
+import { Layout } from "./components/Layout";
 
 export const App = () => {
   return (
-    <>
-      <Header />
-      <Nav />
-      <Footer />
-    </>
+    <Layout>
+      <h1 className="text-2xl text-oow-white">메인 페이지</h1>
+    </Layout>
   );
 };
---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Nav } from "./Nav";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex min-h-screen flex-col bg-oow-navy-700">
+      <Header />
+      <Nav />
+      <main className="flex-1 p-6">{children}</main>
+      <Footer />
+    </div>
+  );
+};
