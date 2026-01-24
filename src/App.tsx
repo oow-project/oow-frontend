cde725273c52ce6feefa3628bb494c5afa38eb32
jwe0516 <jwe0516@naver.com>
2026-01-24 13:56:55 +0900

    feat: Nav 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Nav } from "./components/Nav";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Nav />
     </>
   );
 };
---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+export const Nav = () => {
+  return (
+    <nav className="bg-oow-navy-800 flex gap-6 px-6 py-4">
+      <a href="/" className="text-oow-gray text-sm">
+        영웅 통계
+      </a>
+      <a href="/" className="text-oow-gray text-sm">
+        영웅 정보
+      </a>
+    </nav>
+  );
+};
