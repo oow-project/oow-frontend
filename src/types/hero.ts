4cd0efd59035f5d8e9e5ebbfab5949042256cc39
jwe0516 <jwe0516@naver.com>
2026-01-26 14:39:15 +0900

    feat: 영웅 상세 타입 정의 추가

--- src/types/hero.ts
+++ src/types/hero.ts
@@ -1,5 +1,7 @@
 export type HeroRole = "tank" | "damage" | "support";
 
+export type PerkType = "major" | "minor";
+
 export interface Hero {
   key: string;
   name: string;
@@ -10,4 +12,32 @@ export interface Hero {
 export interface HeroListResponse {
   heroes: Hero[];
   total: number;
+}
+
+export interface Ability {
+  name: string;
+  description: string;
+  icon: string;
+}
+
+export interface Perk {
+  name: string;
+  description: string;
+  icon: string;
+  type: PerkType;
+}
+
+export interface Hitpoints {
+  health: number;
+  armor: number;
+  shields: number;
+  total: number;
+}
+
+export interface HeroDetailResponse extends Hero {
+  hitpoints: Hitpoints;
+  abilities: Ability[];
+  perks: Perk[];
+  counters: Hero[];
+  synergies: Hero[];
 }
