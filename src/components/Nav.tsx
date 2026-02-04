64b1274f89db7b73192bbf63a5c0a4e2cad56648
jwe0516 <jwe0516@naver.com>
2026-02-05 01:37:15 +0900

    feat: NavLink로 변경 및 현재 페이지 밑줄 표시 반영

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export const Nav = () => {
   return (
     <nav className="flex gap-4 md:gap-6 px-4 md:px-6 py-3 md:py-4 bg-oow-navy-800">
-      <Link to="/" className="text-sm text-oow-gray">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `text-sm ${isActive ? "text-oow-white border-b-3 border-oow-orange pb-1" : "text-oow-gray"}`
+        }
+      >
         영웅 통계
-      </Link>
-      <Link to="/heroes" className="text-sm text-oow-gray">
+      </NavLink>
+      <NavLink
+        to="/heroes"
+        className={({ isActive }) =>
+          `text-sm ${isActive ? "text-oow-white border-b-3 border-oow-orange pb-1" : "text-oow-gray"}`
+        }
+      >
         영웅 정보
-      </Link>
+      </NavLink>
     </nav>
   );
 };
